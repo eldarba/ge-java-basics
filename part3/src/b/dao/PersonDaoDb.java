1e64c705a6f3445ed7e1e4bf054e0f17288abe32
jbt <jbt@AIO-2021->
2022-06-01 12:51:36 +0300

    AAA

--- part3/src/b/dao/PersonDaoDb.java
+++ part3/src/b/dao/PersonDaoDb.java
@@ -1,10 +1,36 @@
 package b.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PersonDaoDb implements PersonDao {
 
 	@Override
 	public void add(Person person) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		try {
+			// get a connection from the connection pool
+			con = ConnectionPool.getInstance().getConnection();
+			String sql = "insert into person values(?, ?, ?)";
+			// a PreparedStatement can get parameters as ?
+			try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+				// initialize the PreparedStatement parameters
+				pstmt.setInt(1, person.getId());
+				pstmt.setString(2, person.getName());
+				pstmt.setDate(3, Date.valueOf(person.getBirthdate()));
+				// execute the SQL
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				throw new RuntimeException("add person faild", e);
+			}
+
+		} finally {
+			if (con != null) {
+				ConnectionPool.getInstance().returnConnection(con);
+			}
+		}
 
 	}
 
