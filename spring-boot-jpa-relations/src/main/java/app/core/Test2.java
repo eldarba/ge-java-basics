a7f4a15627e7e7a6f4c93e18eca931c9e7fadcac
jbt <jbt@AIO-2021->
2022-06-12 14:32:43 +0300

    JPA

--- spring-boot-jpa-relations/src/main/java/app/core/Test2.java
+++ spring-boot-jpa-relations/src/main/java/app/core/Test2.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import app.core.entities.Address;
 import app.core.entities.School;
 import app.core.entities.Student;
 import app.core.services.SchoolService;
 
-//@Component
+@Component
 public class Test2 implements CommandLineRunner {
 
 	@Autowired
@@ -18,26 +19,33 @@ public class Test2 implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		try {
+			Student st1 = new Student(0, "aaa", LocalDate.of(1985, 01, 01), null);
+			Student st2 = new Student(0, "bbb", LocalDate.of(2000, 01, 01), null);
+			Student st3 = new Student(0, "ccc", LocalDate.of(2010, 01, 01), null);
+			Student st4 = new Student(0, "ddd", LocalDate.of(1992, 01, 01), null);
+			Student st5 = new Student(0, "eee", LocalDate.of(2005, 01, 01), null);
 
-		Student st1 = new Student(0, "aaa", LocalDate.of(1985, 01, 01), null);
-		Student st2 = new Student(0, "bbb", LocalDate.of(2000, 01, 01), null);
-		Student st3 = new Student(0, "ccc", LocalDate.of(2010, 01, 01), null);
-		Student st4 = new Student(0, "ddd", LocalDate.of(1992, 01, 01), null);
-		Student st5 = new Student(0, "eee", LocalDate.of(2005, 01, 01), null);
+			School school1 = new School(0, "Bar Ilan", new Address(0, "Israel", "Tel Aviv", "Gordon", 100, null), null);
+			School school2 = new School(0, "Ben Gurion",
+					new Address(0, "Israel", "Beer Sheva", "Hahistadroot", 32, null), null);
 
-		School school1 = new School(0, "Bar Ilan", new Address(0, "Israel", "Tel Aviv", "Gordon", 100, null), null);
-		School school2 = new School(0, "Ben Gurion", new Address(0, "Israel", "Beer Sheva", "Hahistadroot", 32, null),
-				null);
+			school1.addStudent(st1);
+			school1.addStudent(st2);
+			school1.addStudent(st3);
 
-		school1.addStudent(st1);
-		school1.addStudent(st2);
-		school1.addStudent(st3);
+			school2.addStudent(st4);
+			school2.addStudent(st5);
 
-		school2.addStudent(st4);
-		school2.addStudent(st5);
+			service.addSchool(school1);
+			service.addSchool(school2);
 
-		service.addSchool(school1);
-		service.addSchool(school2);
+		} catch (Exception e) {
+			System.out.println("======== ERROR ===============");
+			System.out.println("Error: " + e.getMessage());
+			System.out.println("==============================");
+			e.printStackTrace();
+		}
 	}
 
 }
