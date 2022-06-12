6d0d1ecd4964e575a2dfe9393a9cc0a3a51523f7
jbt <jbt@AIO-2021->
2022-06-12 12:14:58 +0300

    JPA

---
+++ spring-boot-jpa-relations/src/main/java/app/core/entities/School.java
@@ -0,0 +1,30 @@
+package app.core.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+public class School {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	@OneToOne
+	@JoinColumn(name = "address_id")
+	private Address address;
+
+}
