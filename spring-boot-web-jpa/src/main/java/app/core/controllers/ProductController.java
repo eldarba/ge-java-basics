125effa9833c44b7ff24f55a040b760c21e851c4
jbt <jbt@AIO-2021->
2022-06-15 14:13:38 +0300

    AAA

--- spring-boot-web-jpa/src/main/java/app/core/controllers/ProductController.java
+++ spring-boot-web-jpa/src/main/java/app/core/controllers/ProductController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,24 @@ public class ProductController {
 	public List<Product> getAllProducts() {
 		try {
 			return this.productService.getAllProducts();
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
+
+	@PutMapping
+	public void updateProduct(@RequestBody Product product) {
+		try {
+			this.productService.updateProduct(product);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
+
+	@DeleteMapping
+	public void deleteProduct(@RequestParam int productId) {
+		try {
+			this.productService.deleteProduct(productId);
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 		}
